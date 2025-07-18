eba7ecf870598a98070eba2f9d53b3082bb5e9dc
NyeinPyaeSone07 <68011421@kmitl.ac.th>
2025-07-18 08:54:15 +0000

    lab1

---
+++ lab-1/ex01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf ("Hello, World!$\n");
+    printf ("Welcome to Robotics and AI!$\n");
+
+   
+
+    return 0;
+}
---
+++ lab-1/ex03.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf ("%d\n",(97+21)-56);
+
+    return 0;
+}
